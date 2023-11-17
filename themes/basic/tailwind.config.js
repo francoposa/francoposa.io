bb8541c376a61240591b01bab3d29a84a169bbee
francoposa <franco@francoposa.io>
2023-11-17 15:14:14 -0800

    make header fonts bolder; default to UI font theme for non-header text

--- themes/basic/tailwind.config.js
+++ themes/basic/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
         mono: ['Courier Prime', ...defaultTheme.fontFamily.mono],
       },
     },
